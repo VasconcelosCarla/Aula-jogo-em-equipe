2446bac117bad24af0b93dbb63b3b32ce3feae12
lucs-max <luccaandradebonifacio@gmail.com>
2025-06-16 18:34:21 -0400

    adicionei a animaçao e criei o canvas

--- sketch.js
+++ sketch.js
@@ -1,14 +1,17 @@
 var bola, bolaAnimation;
 
 function preload(){
-    bolaAnimation = loadAnimation("../assets/sprite_0.png", "../assets/sprite_1.png");
+    bolaAnimation = loadAnimation("../assets/sprite_0.png", "../assets/sprite_1.png","../assets/sprite_2.png","../assets/sprite_3.png");
 }
 
 function setup(){
+    createCanvas(1535,695);
     bola = createSprite(200, 200);
-
+    bola.addAnimation("piscando",bolaAnimation);
+    bola.scale = 3
 }
 
 function draw() {
+    background("gray");
     drawSprites();
 }
